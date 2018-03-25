2781907c13978742cf41f2c5bd52a0d8e5b37e22
Sean Ruhland <sean.ruhland@gmail.com>
2018-03-25 15:11:02 -0700

    ajax

--- app/components/App.js
+++ app/components/App.js
@@ -1,18 +1,12 @@
 var React = require('react');
-var Popular = require('./Popular')
-var ReactRouter = require('react-router-dom');
-var Router = ReactRouter.BrowserRouter;
-var Route = ReactRouter.Route;
-
+var Popular = require('./Popular');
 
 class App extends React.Component {
   render() {
     return (
-    	<Router>
-	    	<div className='container'>
-	    		<Route path='/popular' component={Popular} />
-	    	</div>
-    	</Router>
+      <div className='container'>
+        <Popular />
+      </div>
     )
   }
 }
--- app/index.js
+++ app/index.js
@@ -1,9 +1,7 @@
-
 var React = require('react');
 var ReactDOM = require('react-dom');
 require('./index.css');
-var App = require('./components/App')
-
+var App = require('./components/App');
 
 ReactDOM.render(
   <App />,
